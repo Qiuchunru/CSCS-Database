c895df4338d4e7dc8742d6b9c09bdb2d123d2ba0
Aaida Hossain <aaida.hossain1@gmail.com>
2026-07-19 16:52:59 -0400

    Refactor SQL query for major FIFA game participants
    Refactored SQL query to select major members who played FIFA games, consolidating fields and improving readability.

--- SQL/queries/Major members who played FIFA games.sql
+++ SQL/queries/Major members who played FIFA games.sql
@@ -1,67 +1,37 @@
-USE CSCS;
+-- Query ii: major members who played in at least one FIFA game
+USE wec353_1;
+
 SELECT
+    L.Name AS Location_Name,
+    CM.MemberID,
+    CM.FirstName,
+    CM.LastName,
+    TIMESTAMPDIFF(YEAR, CM.DOB, CURDATE()) AS Age,
+    CM.City,
+    CM.Province,
 
-L.Name AS Location_Name,
+    CASE
+        WHEN (SELECT COALESCE(SUM(P.Amount), 0)
+              FROM Payments P
+              WHERE P.MemberID = CM.MemberID
+                AND P.MembershipYear = YEAR(CURDATE()) - 1)
+             >= (CASE WHEN CM.MemberType = 'Major' THEN 200 ELSE 100 END)
+        THEN 'Active'
+        ELSE 'Inactive'
+    END AS Status,
 
-CM.MemberID,
-
-CM.FirstName,
-
-CM.LastName,
-
-
-TIMESTAMPDIFF(
-YEAR,
-CM.DOB,
-CURDATE()
-)
-
-AS Age,
-
-
-CM.City,
-
-CM.Province,
-
-
-'Active' AS Status,
-
-
-COUNT(GP.GameID)
-
-AS FIFA_Game_Number
-
+    COUNT(GP.GameID) AS FIFA_Game_Number
 
 FROM ClubMembers CM
-
-
 JOIN Member_Location_History MLH
-
-ON CM.MemberID=MLH.MemberID
-
-
+     ON CM.MemberID = MLH.MemberID AND MLH.EndDate IS NULL
 JOIN Locations L
-
-ON MLH.LocationID=L.LocationID
-
-
+     ON MLH.LocationID = L.LocationID
 JOIN Game_Participation GP
+     ON CM.MemberID = GP.MemberID
 
-ON CM.MemberID=GP.MemberID
+WHERE CM.MemberType = 'Major'
 
-
-WHERE CM.MemberType='Major'
-
-
-GROUP BY
-
-CM.MemberID,
-L.Name,
-CM.FirstName,
-CM.LastName,
-CM.DOB,
-CM.City,
-CM.Province
-
+GROUP BY CM.MemberID, L.Name
 
 ORDER BY FIFA_Game_Number ASC;
