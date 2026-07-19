d1664d49a44580cf70d9923c2b1db87ca5d2f511
Aaida Hossain <aaida.hossain1@gmail.com>
2026-07-19 16:56:04 -0400

    Refactor SQL query for major members payments and donations

--- SQL/queries/2023-2025 Major members payment_donation.sql
+++ SQL/queries/2023-2025 Major members payment_donation.sql
@@ -1,50 +1,15 @@
--- Query vii
-
+-- Query vii: sum of membership fees and donations from major members, 2023-2025
+-- The yearly fee for a major member is 200$; anything paid above that within a membership year counts as a donation.
+USE wec353_1;
 
 SELECT
-
-
-SUM(
-CASE
-
-WHEN P.Amount <= 200
-
-THEN P.Amount
-
-ELSE 200
-
-END
-)
-
-AS Total_Membership_Fees,
-
-
-SUM(
-CASE
-
-WHEN P.Amount > 200
-
-THEN P.Amount - 200
-
-ELSE 0
-
-END
-)
-
-AS Total_Donations
-
-
-
-FROM Payments P
-
-
-JOIN ClubMembers CM
-
-ON P.MemberID = CM.MemberID
-
-
-
-WHERE CM.MemberType='Major'
-
-
-AND P.MembershipYear BETWEEN 2023 AND 2025;
+    SUM(LEAST(T.TotalPaid, 200))        AS Total_Membership_Fees,
+    SUM(GREATEST(T.TotalPaid - 200, 0)) AS Total_Donations
+FROM (
+    SELECT P.MemberID, P.MembershipYear, SUM(P.Amount) AS TotalPaid
+    FROM Payments P
+    JOIN ClubMembers CM ON P.MemberID = CM.MemberID
+    WHERE CM.MemberType = 'Major'
+      AND P.MembershipYear BETWEEN 2023 AND 2025
+    GROUP BY P.MemberID, P.MembershipYear
+) AS T;
